2cec25507faa75d956e66ee83af61cbeef7a4195
Zixia Zhang <Zixia.Zhang@zopa.com>
2016-11-19 02:50:47 +0000

    Wiring up the handler to the database

---
+++ dojo/dojo.server/Controllers/OverviewController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using dojo.server.Models;
+using Dapper;
+
+namespace dojo.server.Controllers
+{
+    public class OverviewController : ApiController
+    {
+        private string connectionString;
+
+        private const string GetScore = @"
+            SELECT Score
+            FROM Overview";
+
+        private const string UpdateScore = @"
+            UPDATE Overview
+            SET Score = Score + @Amount,
+                Updated = GETDATE()";
+
+        public OverviewController()
+        {
+            this.connectionString = ConfigurationManager.ConnectionStrings["Dojo"].ConnectionString;
+        }
+        public DojoOverview GetOverview()
+        {
+            using (var conn = new SqlConnection(this.connectionString))
+            {
+                var overview =
+                    conn.Query(GetScore)
+                        .Select(
+                            row =>
+                                new DojoOverview
+                                {
+                                    Score = row.Score
+                                })
+                        .First();
+                return overview;
+            }
+        }
+
+        public HttpResponseMessage PostScore([FromBody]int amount)
+        {
+            using (var conn = new SqlConnection(this.connectionString))
+            {
+                conn.Execute(
+                            UpdateScore,
+                            new
+                            {
+                                Amount = amount
+                            });
+            }
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+    }
+}
