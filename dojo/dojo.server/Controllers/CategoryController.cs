517b72a089c527994607152ed287864cfe82f446
lolbro-consulting <lolbro@outlook.com>
2016-11-19 09:58:31 +0100

    Adding CategoryController

---
+++ dojo/dojo.server/Controllers/CategoryController.cs
@@ -0,0 +1,32 @@
+﻿using System.Web.Http;
+
+namespace dojo.server.Controllers
+{
+    public class CategoryController : ApiController
+    {
+        public decimal GetAmmoutForCategory(string category)
+        {
+            switch (category)
+            {
+                case "Entertainment/Recreation":
+                    return 120;
+                case "Service Charges/Fees":
+                    return 200;
+                case "Electronics/General Merchandise":
+                    return 150;
+                case "Restaurants":
+                    return 15;
+                case "Home Improvement":
+                    return 500;
+                case "Automotive/Fuel":
+                    return 600;
+                case "Cable/Satellite/Telecom":
+                    return 100;
+                case "Credit Card Payments":
+                    return 10000;
+            }
+
+            return 156;
+        }     
+    }
+}
