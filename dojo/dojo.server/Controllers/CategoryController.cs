3331c1f30d6c6c569afff6280eb4cbc1f6711b4c
lolbro-consulting <lolbro@outlook.com>
2016-11-19 10:45:21 +0100

    CategoryController takes time interval into account when providing the amount

--- dojo/dojo.server/Controllers/CategoryController.cs
+++ dojo/dojo.server/Controllers/CategoryController.cs
@@ -1,29 +1,44 @@
-﻿using System.Web.Http;
+﻿using System;
+using System.Web.Http;
 
 namespace dojo.server.Controllers
 {
     public class CategoryController : ApiController
     {
-        public decimal GetAmmoutForCategory(string category)
+        public CategoryController()
         {
+        }
+
+        public decimal GetAmmoutForCategory(string category, int timeInterval)
+        {
+            if (category == null)
+            {
+                throw new ArgumentNullException(nameof(category));
+            }
+
+            if (string.IsNullOrWhiteSpace(category))
+            {
+                throw new ArgumentException(nameof(category));
+            }
+
             switch (category)
             {
                 case "Entertainment/Recreation":
-                    return 120;
+                    return 120 * timeInterval;
                 case "Service Charges/Fees":
-                    return 200;
+                    return 200 * timeInterval;
                 case "Electronics/General Merchandise":
-                    return 150;
+                    return 150 * timeInterval;
                 case "Restaurants":
-                    return 15;
+                    return 15 * timeInterval;
                 case "Home Improvement":
-                    return 500;
+                    return 500 * timeInterval;
                 case "Automotive/Fuel":
-                    return 600;
+                    return 600 * timeInterval;
                 case "Cable/Satellite/Telecom":
-                    return 100;
+                    return 100 * timeInterval;
                 case "Credit Card Payments":
-                    return 10000;
+                    return 10000 * timeInterval;
             }
 
             return 156;
