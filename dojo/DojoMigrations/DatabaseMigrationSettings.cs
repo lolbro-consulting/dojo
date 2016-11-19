015ad37f388f6c267415fa9637f0dbebb6617788
Zixia Zhang <Zixia.Zhang@zopa.com>
2016-11-19 00:47:27 +0000

    Adding data migration

---
+++ dojo/DojoMigrations/DatabaseMigrationSettings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DojoMigrations
+{
+    public class DatabaseMigrationSettings
+    {
+        public const string SchemaScriptsFolder = "DojoMigrations.Schema";
+
+        public static Assembly ScriptsAssembly => typeof(DatabaseMigrationSettings).Assembly;
+    }
+}
---
+++ dojo/dojo.server/Models/DojoOverview.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace dojo.server.Models
+{
+    public class DojoOverview
+    {
+        public int Score { get; set; }
+    }
+}
